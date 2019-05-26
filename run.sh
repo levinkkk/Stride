4012c0b1701ee1271850fdcd11de9242fd8f873c
Paul MacRory <pmacro@icloud.com>
2019-05-26 12:17:48 -0700

    Tidy up structure/unneeded files

--- run.sh
+++
@@ -1,3 +0,0 @@
-YOGA_LIB_PATH=$(find . -wholename '*/Sources/Yoga' | head -n 1)
-echo "Using Yoga in: $YOGA_LIB_PATH"
-swift run -Xswiftc "-target" -Xswiftc "x86_64-apple-macos10.13" -Xlinker -lc++ -Xswiftc -L$YOGA_LIB_PATH
